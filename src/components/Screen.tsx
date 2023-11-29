1c749d3f00252e8cb94fcb1a03395ca0b882547b
Intrstng <intrstg@gmail.com>
2023-11-29 10:50:28 +0300

    feat: complete task - conditional rendering in Screen component without Styled Components

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,7 +1,46 @@
-// На Styled components:
+// // На Styled components:
+//
+// import React, {FC} from 'react';
+// import styled, { css } from 'styled-components';
+//
+// type ScreenPropsType = {
+//     value: number
+// }
+//
+// export const Screen: FC<ScreenPropsType> = ({value}) => {
+//     return (
+//         <ScreenContent value={value}>
+//             <CounterDigit>{value}</CounterDigit>
+//         </ScreenContent>
+//     );
+// };
+//
+//
+// const ScreenContent = styled.div<ScreenPropsType> `
+//   ${props => props.value === 5 && css<ScreenPropsType> `
+//      color: red;
+//   `};
+//
+//   margin: 0 auto 20px;
+//   width: 100%;
+//   height: 150px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border: 2px solid #385f5d;
+//   border-radius: 20px;
+//   background-color: #65cff4;
+// `
+//
+// const CounterDigit = styled.span `
+//   font-size: 40px;
+//   font-family: 'Comic Sans MS';
+// `
+
 
 import React, {FC} from 'react';
 import styled, { css } from 'styled-components';
+import S from './Screen.module.css';
 
 type ScreenPropsType = {
     value: number
@@ -9,18 +48,14 @@ type ScreenPropsType = {
 
 export const Screen: FC<ScreenPropsType> = ({value}) => {
     return (
-        <ScreenContent value={value}>
-            <CounterDigit>{value}</CounterDigit>
+        <ScreenContent>
+            <CounterDigit className={value === 5 ? S.overlimit : S.primary }>{value}</CounterDigit>
         </ScreenContent>
     );
 };
 
 
-const ScreenContent = styled.div<ScreenPropsType> `
-  ${props => props.value === 5 && css<ScreenPropsType> `
-     color: red;
-  `};
-
+const ScreenContent = styled.div `
   margin: 0 auto 20px;
   width: 100%;
   height: 150px;
@@ -33,8 +68,4 @@ const ScreenContent = styled.div<ScreenPropsType> `
 `
 
 const CounterDigit = styled.span `
-  font-size: 40px;
-  font-family: 'Comic Sans MS';
 `
-
-
